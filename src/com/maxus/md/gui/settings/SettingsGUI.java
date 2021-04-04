dfbddc3ef84f2c904fa6bbfd18ad633440aeb6b1
Maksim <maxus.info.mail@gmail.com>
2021-04-04 19:43:07 +0300

    nah

--- src/com/maxus/md/gui/settings/SettingsGUI.java
+++ src/com/maxus/md/gui/settings/SettingsGUI.java
@@ -23,105 +23,73 @@ public class SettingsGUI {
         } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
             e.printStackTrace();
         }
-
         JFrame frame = new JFrame("MD Settings");
-        /* Should look something like that:
-                 UT
-        LUP     CUP     RUP
-
-        LCP     CCP     RCP
-
-        LBP     CBP     RBP
-                BP
-         */
-
-        // panels
-        // upper panel
-        JPanel upper = new JPanel();
-
-        // left panels
         JPanel leftPanel = new JPanel();
-        JPanel leftUpperPanel = new JPanel();
-        JPanel leftCentralPanel = new JPanel();
-        JPanel leftBottomPanel = new JPanel();
-
-        // central panels
         JPanel centralPanel = new JPanel();
-        JPanel centralUpperPanel = new JPanel();
-        JPanel centralCentralPanel = new JPanel();
-        JPanel centralBottomPanel = new JPanel();
-
-        // right panels
-        JPanel rightPanel = new JPanel();
-        JPanel rightUpperPanel = new JPanel();
-        JPanel rightCentralPanel = new JPanel();
-        JPanel rightBottomPanel = new JPanel();
-
-        // text labels
-        JLabel sTXT = new JLabel("Settings");
-
-        // buttons
-
-        JButton lupTXT = new JButton("Fullscreen Mode");
-        JButton lcpTXT = new JButton("Remember users");
-        JButton lbpTXT = new JButton("Language");
-
-        JButton cupTXT = new JButton("filler");
-        JButton ccpTXT = new JButton("filler");
-        JButton cbpTXT = new JButton("filler");
-
-        JButton rupTXT = new JButton("filler");
-        JButton rcpTXT = new JButton("filler");
-        JButton rbpTXT = new JButton("filler");
-
-        // adding
-
-        // elements
-        upper.add(sTXT);
-
-        leftUpperPanel.add(lupTXT);
-        leftCentralPanel.add(lcpTXT);
-        leftBottomPanel.add(lbpTXT);
-
-        centralUpperPanel.add(cupTXT);
-        centralCentralPanel.add(ccpTXT);
-        centralBottomPanel.add(cbpTXT);
-
-        rightUpperPanel.add(rupTXT);
-        rightCentralPanel.add(rcpTXT);
-        rightBottomPanel.add(rbpTXT);
-
-        // panels
-
-        leftPanel.add(leftUpperPanel, BorderLayout.NORTH);
-        leftPanel.add(leftCentralPanel, BorderLayout.CENTER);
-        leftPanel.add(leftBottomPanel, BorderLayout.SOUTH);
-
-        centralPanel.add(centralUpperPanel, BorderLayout.NORTH);
-        centralPanel.add(centralCentralPanel, BorderLayout.CENTER);
-        centralPanel.add(centralBottomPanel, BorderLayout.SOUTH);
-
-        rightPanel.add(rightUpperPanel, BorderLayout.NORTH);
-        rightPanel.add(rightCentralPanel, BorderLayout.CENTER);
-        rightPanel.add(rightBottomPanel, BorderLayout.SOUTH);
-
-        // frame
-
-        frame.getContentPane().add(BorderLayout.PAGE_START, leftPanel);
+        String[] screens = {"Fullscreen", "Windowed"};
+        JComboBox<String> lup = new JComboBox<>(screens);
+        JLabel lup1 = new JLabel("Mode:");
+        String[] users = {"ON", "OFF"};
+        JComboBox<String> lcp = new JComboBox<>(users);
+        JLabel lcp1 = new JLabel("Remember users:");
+        String[] languages = {"English", "Русский"};
+        JComboBox<String> lbp = new JComboBox<>(languages);
+        JLabel lbp1 = new JLabel("Language:");
+        String[] choices = {"Unimaginable", "Very high", "High", "Normal", "Low", "Super Low", "My PC Is A Potato"};
+        JComboBox<String> cup = new JComboBox<>(choices);
+        JLabel cup1 = new JLabel("Graphics:");
+        String[] ch2 = {"Dark", "Light"};
+        JComboBox<String> ccp = new JComboBox<>(ch2);
+        JLabel ccp1 = new JLabel("Theme:");
+        JTextField cbp = new JTextField(10);
+        JLabel cbp1 = new JLabel("Name:");
+        JTextField rup = new JTextField(11);
+        JLabel rup1 = new JLabel("Secret code:");
+        JButton bottom = new JButton("Send!");
+        JPanel btm = new JPanel();
+        btm.add(bottom);
+        JPanel pan1 = new JPanel();
+        JPanel pan2 = new JPanel();
+        JPanel pan3 = new JPanel();
+        JPanel pan4 = new JPanel();
+        JPanel pan5 = new JPanel();
+        JPanel pan6 = new JPanel();
+        JPanel pan7 = new JPanel();
+        pan1.add(lup1);
+        pan1.add(lup);
+        pan2.add(lcp1);
+        pan2.add(lcp);
+        pan3.add(lbp1);
+        pan3.add(lbp);
+        pan4.add(cup1);
+        pan4.add(cup);
+        pan5.add(ccp1);
+        pan5.add(ccp);
+        pan6.add(cbp1);
+        pan6.add(cbp);
+        pan7.add(rup1);
+        pan7.add(rup);
+        JPanel leftM = new JPanel();
+        leftM.add(pan1, BorderLayout.NORTH);
+        leftM.add(pan2, BorderLayout.CENTER);
+        leftM.add(pan3, BorderLayout.SOUTH);
+        JPanel cenM = new JPanel();
+        cenM.add(pan4, BorderLayout.NORTH);
+        cenM.add(pan5, BorderLayout.CENTER);
+        cenM.add(pan6, BorderLayout.SOUTH);
+        leftPanel.add(leftM, BorderLayout.NORTH);
+        centralPanel.add(cenM, BorderLayout.CENTER);
+        centralPanel.add(pan7, BorderLayout.SOUTH);
+        frame.getContentPane().add(BorderLayout.NORTH, leftPanel);
         frame.getContentPane().add(BorderLayout.CENTER, centralPanel);
-        frame.getContentPane().add(BorderLayout.PAGE_END, rightPanel);
-        frame.getContentPane().add(BorderLayout.LINE_END, upper);
-
-        // frame configs
-
+        frame.getContentPane().add(BorderLayout.SOUTH, btm);
         String pathToIcon = "/com/maxus/md/logging/icon.png";
         System.out.println(pathToIcon);
-
         URL iconURL = GUI.class.getResource(pathToIcon);
         ImageIcon icon = new ImageIcon(iconURL);
         frame.setIconImage(icon.getImage());
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(700, 700);
+        frame.setSize(500, 230);
         frame.setVisible(true);
     }
     @SuppressWarnings("unused")
